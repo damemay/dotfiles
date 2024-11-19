22fa2f79e09fbaa7a1af715d64c018deb67455e8
mar <arch-legion>
2024-11-19 12:24:47 +0100

    perform PlugInstall and vim symlink

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ make -C $REPO_DIR/ble.sh install PREFIX=$HOME/.local
 mkdir -p $HOME/.config/i3 && cp $REPO_DIR/i3-config $_/config
 mkdir -p $HOME/.config/kitty && cp $REPO_DIR/kitty.conf $_/
 mkdir -p $HOME/.config/polybar && cp $REPO_DIR/polybar-config.ini $_/config.ini
+sudo ln -s /usr/bin/nvim /usr/bin/vim
 mkdir -p $HOME/.config/nvim && cp $REPO_DIR/init.vim $_/
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+/usr/bin/nvim +PlugInstall +qa
