0386439e562ed00c033c07b166760f6153dff662
march <mar@march>
2024-12-27 10:08:14 +0100

    update

--- install.sh
+++ install.sh
@@ -2,19 +2,21 @@
 # This script assumes Arch Linux with X11.
 # Make sure all submodules are cloned.
 
-REPO_DIR=$(dirname "$0")
+REPO_DIR=$(dirname -- "$(readlink -f -- "$0")")
 
 pacman -S - < $REPO_DIR/pacman_packages
-sudo cp /etc/X11/xinit/xinitrc $HOME/.xinitrc
-cat $REPO_DIR/.xinitrc >> $HOME/.xinitrc
-cp $REPO_DIR/.bash_profile $HOME/
-cp $REPO_DIR/.bashrc $HOME/
-make -C $REPO_DIR/ble.sh install PREFIX=$HOME/.local
-mkdir -p $HOME/.config/i3 && cp $REPO_DIR/i3-config $_/config
-mkdir -p $HOME/.config/kitty && cp $REPO_DIR/kitty.conf $_/
-mkdir -p $HOME/.config/polybar && cp $REPO_DIR/polybar-config.ini $_/config.ini
-sudo ln -s /usr/bin/nvim /usr/bin/vim
-mkdir -p $HOME/.config/nvim && cp $REPO_DIR/init.vim $_/
+
+sudo ln -sf $REPO_DIR/xorg-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
+ln -sf $REPO_DIR/.xinitrc $HOME/.xinitrc
+ln -sf $REPO_DIR/.bash_profile $HOME/
+ln -sf $REPO_DIR/.bashrc $HOME/
+mkdir -p $HOME/.config/i3 && ln -sf $REPO_DIR/i3-config $_/config
+mkdir -p $HOME/.config/kitty && ln -sf $REPO_DIR/kitty.conf $_/
+mkdir -p $HOME/.config/polybar && ln -sf $REPO_DIR/polybar-config.ini $_/config.ini
+mkdir -p $HOME/.config/nvim && ln -sf $REPO_DIR/init.vim $_/
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 /usr/bin/nvim +PlugInstall +qa
+
+make -C $REPO_DIR/ble.sh install PREFIX=$HOME/.local
+sudo ln -s /usr/bin/nvim /usr/bin/vim
