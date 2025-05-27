662b8f21cafd334d947f11ba7d2c379e6ef1d5c9
mar <wsl>
2025-05-27 17:54:58 +0200

    wsl2 basic setup

--- .bash_profile
+++ .bash_profile
@@ -1,5 +1,1 @@
-sudo rmmod pcspkr
 [[ -f ~/.bashrc ]] && . ~/.bashrc
-if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-    exec startx -- -dpi 96
-fi
--- install.sh
+++ install.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 # This script assumes Arch Linux with X11.
 # Make sure all submodules are cloned.
-# Run with sudo.
 
 REPO_DIR=$(dirname -- "$(readlink -f -- "$0")")
 
-pacman -S - < $REPO_DIR/pacman-packages
+sudo pacman -S - < $REPO_DIR/pacman-packages
 
-ln -sf $REPO_DIR/xorg-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
-ln -sf $REPO_DIR/xorg-keyboard.conf /etc/X11/xorg.conf.d/10-keyboard.conf
-ln -sf $REPO_DIR/.xinitrc $HOME/.xinitrc
-ln -sf $REPO_DIR/.bash_profile $HOME/
-ln -sf $REPO_DIR/.bashrc $HOME/
-mkdir -p $HOME/.config/i3 && ln -sf $REPO_DIR/i3-config $_/config
-mkdir -p $HOME/.config/kitty && ln -sf $REPO_DIR/kitty.conf $_/
-mkdir -p $HOME/.config/polybar && ln -sf $REPO_DIR/polybar-config.ini $_/config.ini
-mkdir -p $HOME/.config/nvim && ln -sf $REPO_DIR/init.vim $_/
+sudo ln -sf $REPO_DIR/.bash_profile $HOME/
+sudo ln -sf $REPO_DIR/.bashrc $HOME/
+mkdir -p $HOME/.config/nvim && sudo ln -sf $REPO_DIR/init.vim $_/
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 /usr/bin/nvim +PlugInstall +qa
