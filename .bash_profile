28c476bceabdf2be49cbba6355640a424826cda0
march <leg5>
2025-06-05 19:38:31 +0200

    up

--- .bash_profile
+++ .bash_profile
@@ -1,4 +1,3 @@
-sudo rmmod pcspkr
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
     exec startx -- -dpi 96
--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 # This script assumes Arch Linux with X11.
 # Make sure all submodules are cloned.
-# Run with sudo.
 
 REPO_DIR=$(dirname -- "$(readlink -f -- "$0")")
 
-pacman -S - < $REPO_DIR/pacman-packages
+sudo pacman -S - < $REPO_DIR/pacman-packages
 
-ln -sf $REPO_DIR/xorg-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
-ln -sf $REPO_DIR/xorg-keyboard.conf /etc/X11/xorg.conf.d/10-keyboard.conf
+sudo cp $REPO_DIR/20-wireless.network /etc/systemd/network/20-wireless.network
+sudo systemctl enable systemd-resolved
+sudo systemctl enable systemd-networkd
+sudo systemctl enable iwd
+
+sudo ln -sf $REPO_DIR/xorg-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
+sudo ln -sf $REPO_DIR/xorg-keyboard.conf /etc/X11/xorg.conf.d/10-keyboard.conf
+
 ln -sf $REPO_DIR/.xinitrc $HOME/.xinitrc
 ln -sf $REPO_DIR/.bash_profile $HOME/
 ln -sf $REPO_DIR/.bashrc $HOME/
+
 mkdir -p $HOME/.config/i3 && ln -sf $REPO_DIR/i3-config $_/config
 mkdir -p $HOME/.config/kitty && ln -sf $REPO_DIR/kitty.conf $_/
 mkdir -p $HOME/.config/polybar && ln -sf $REPO_DIR/polybar-config.ini $_/config.ini
@@ -21,4 +27,3 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 /usr/bin/nvim +PlugInstall +qa
 
 make -C $REPO_DIR/ble.sh install PREFIX=$HOME/.local
-ln -s /usr/bin/nvim /usr/bin/vim
